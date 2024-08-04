9283fa4aceee7c1055390ee281f2dc638d82aeee
FurWaz <fur.waz06@gmail.com>
2024-08-04 22:28:49 +0200

    Removed unfinished routes from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,11 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/',                name: 'Home',           component: HomeView,                                        beforeEnter: () => true },
-        { path: '/privacy',         name: 'Privacy',        component: () => import("@/views/LegalDocView.vue"),       beforeEnter: () => true },
-        { path: '/terms',           name: 'Terms',          component: () => import("@/views/LegalDocView.vue"),       beforeEnter: () => true },
+        // { path: '/privacy',         name: 'Privacy',        component: () => import("@/views/LegalDocView.vue"),       beforeEnter: () => true },
+        // { path: '/terms',           name: 'Terms',          component: () => import("@/views/LegalDocView.vue"),       beforeEnter: () => true },
 
         // 404 redirect
-        { path: "/:catchAll(.*)", name: 'NotFound', component: () => import("@/views/NotFound.vue") }
+        // { path: "/:catchAll(.*)", name: 'NotFound', component: () => import("@/views/NotFound.vue") }
     ]
 });
 
